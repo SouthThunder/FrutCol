07fa10d39f46e33a3497436c73998831a61eb059
CarlosLopez <carlosgablopezm@gmail.com>
2023-08-20 19:25:00 -0500

    Css header

--- frontend/src/header/header.jsx
+++ frontend/src/header/header.jsx
@@ -4,8 +4,27 @@ import './header.css'
 export default function Headercom(){
 
     return(
-        <div className="headercontain">
-            <p className="holahead">hola soy el header</p>
+        <div className="header">
+           
+           <nav className="nav container">
+           <a href="#" className="nav__logo">
+                <img src="images/Frame 1.png" alt="" />
+           </a>
+           <div className="nav__menu" id="nav-menu">
+            <ul className="nav__list">
+                <li className="nav__item inicios">
+                    <a href="" className="nav__link"id="inicios">Iniciar Sesión</a>
+                </li>
+                <li className="nav__item">
+                    <a href="" className="nav__link">Registrarse</a>
+                </li>
+                <li className="nav__item">
+                    <img src="images/carrito.png" alt="" />
+                </li>
+            </ul>
+           </div>
+           </nav>
         </div>
-    )
+
+    )   
 }
