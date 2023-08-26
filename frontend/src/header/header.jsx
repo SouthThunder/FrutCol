99be61a81b12990600c17d7323e949215a59c2b4
CarlosLopez <carlosgablopezm@gmail.com>
2023-08-26 12:42:39 -0500

    feature: routes signin and signup

--- frontend/src/header/header.jsx
+++ frontend/src/header/header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "./header.css";
+import { Navigate, useNavigate } from "react-router-dom";
 
 export const Headercom = () => {
+  const navigate=useNavigate();
+  const logIn = () =>{
+    navigate("/Ingreso")
+  }
+  const signUp = () =>{
+    navigate("/Registro")
+  }
+
   return (
     <div className="header">
       <nav className="nav-bar">
@@ -12,10 +21,10 @@ export const Headercom = () => {
         <div className="nav__menu">
           <ul className="nav__list">
             <li className="nav__item">
-              <button className="nav__link">Registrarse</button>
+              <button className="nav__link" onClick={signUp}>Registrarse</button>
             </li>
             <li className="nav__item">
-              <button className="nav__link">Iniciar sesión</button>
+              <button className="nav__link" onClick={logIn}>Iniciar sesión</button>
             </li>
             <li className="nav__item">
               <img src="images/carrito.png" />
