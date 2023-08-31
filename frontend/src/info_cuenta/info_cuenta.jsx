2266326a8e28b3980532a217ee3298885ded0fff
CarlosLopez <carlosgablopezm@gmail.com>
2023-08-30 20:33:11 -0500

    fixed: modify datos page design

--- frontend/src/info_cuenta/info_cuenta.jsx
+++ frontend/src/info_cuenta/info_cuenta.jsx
@@ -5,11 +5,8 @@ import { Footercom } from "../footer/footer";
 import { fresa } from "../home/sliderProds";
 export const Infocuenta = () => {
   return (
-    <div className="infocuenta">
-      <div className="contenidos">
-        <h4>Información General</h4>
-        <h4>Cambio de contraseña</h4>
-      </div>
+    
+      
       <div className="infogeneral">
         <form action="" className="form">
         <div className="input__info">
@@ -73,8 +70,70 @@ export const Infocuenta = () => {
         </div>
         </form>
       </div>
+   
+  );
+};
+export const Infocontenidos = (prop) => {
+  return(
+    <div className="contenidos">
+      <h4 onClick={() => prop.onSelectOption("infocuenta")} style={{ backgroundColor: prop.selectedOption === "infocuenta" ? "#f2f2f2" : "transparent" }}>Información General</h4>
+      <h4 onClick={() => prop.onSelectOption("cambiocontraseña")} style={{ backgroundColor: prop.selectedOption === "cambiocontraseña" ? "#f2f2f2" : "transparent" }}>Cambio de contraseña</h4>
+    </div>
+    
+  );
+
+};
+export const Informacioncuenta = (prop) => {
+  const [selectedOption, setSelectedOption] = useState("infocuenta"); // Por defecto muestra "infocuenta"
+
+  const handleOptionChange = (option) => {
+    setSelectedOption(option);
+  };
+  return(
+    <div className="infocuenta">
+      <Infocontenidos onSelectOption={handleOptionChange} selectedOption={selectedOption} />
+      {selectedOption === "infocuenta" ? (
+        <Infocuenta prod={prop}/>
+      ) : selectedOption === "cambiocontraseña" ? (
+        <Cambiocontraseña prod={prop} />
+      ) : null}
+    </div>
+  )
+}
+export const Cambiocontraseña = (prop) => {
+  return(
+    <div className="cambiocontrasena">
+      <form action="" className="form">
+      <div className="input__info">
+          <small className="errores">Error message</small>
+          <ion-icon name="person"></ion-icon>
+          <input
+            // placeholder={user.nombre}
+            type="password"
+            id="contrasena"
+            name="contrasena"
+          />
+          <label for="">Nueva contraseña</label>
+        </div>
+        <div className="input__info">
+          <small className="errores">Error message</small>
+          <ion-icon name="person"></ion-icon>
+          <input
+            // placeholder={user.nombre}
+            type="password"
+            id="newcontrasena"
+            name="newcontrasena"
+          />
+          <label for="">Confirmar nueva contraseña</label>
+        </div>
+        <br />
+        <div className="enter">
+          <button>Actualizar</button>
+        </div>
+      </form>
     </div>
   );
+
 };
 
 export const InfoCuentacom = (prop) => {
@@ -85,7 +144,7 @@ export const InfoCuentacom = (prop) => {
   return (
     <div className="infoCuentacontain">
       <Headercom prod={product} />
-      <Infocuenta prod={product} />
+      <Informacioncuenta prod={product} />
       <Footercom prod={product} />
     </div>
   );
