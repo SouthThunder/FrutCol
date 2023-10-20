b21c3e73b4c237002a25064fb74b9f4f6831a24e
Luis Alejandro Vera Moreno <alejandrovemo@gmail.com>
2023-10-20 02:00:20 -0500

    basic checkout structure

--- frontend/src/carrito/carrito.jsx
+++ frontend/src/carrito/carrito.jsx
@@ -3,11 +3,29 @@ import './carrito.css'
 import { Headercom } from "../header/header";
 import { Footercom } from "../footer/footer";
 
+export const Cart = ({}) =>{
+    return(
+        <div className="cartComp">
+            <div className="items">
+                <div className="title">
+                    <h1>Carrito</h1>
+                </div>
+                <div className="elements">
+
+                </div>
+            </div>
+            <div className="checkout">
+                <p>Checkout</p>
+            </div>
+        </div>
+    )
+}
+
 export const Carritocom = ({product}) =>{
     return(
         <div className="carritocontain" id="home">
             <Headercom product={product}/>
-            <p>hola soy el carrito</p>
+            <Cart/>
             <Footercom product={product}/>
         </div>
     )
