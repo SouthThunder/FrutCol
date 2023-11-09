094798a446015dd5ffd1499dcf9160e7372ad08d
Luis Alejandro Vera Moreno <alejandrovemo@gmail.com>
2023-11-08 22:54:20 -0500

    feature: add responsiveness to who are we

--- frontend/src/quienes_somos/quienes_somos.jsx
+++ frontend/src/quienes_somos/quienes_somos.jsx
@@ -61,53 +61,67 @@ export const QuienesSomos = ({ product }) => {
         <ul className="objectives__list">
           <li>
             <p>
-            	La pulpa de fruta fresca envasada y congelada conserva sus nutrientes, sabor, aroma y color de la fruta de la cual es extraída.
+              La pulpa de fruta fresca envasada y congelada conserva sus
+              nutrientes, sabor, aroma y color de la fruta de la cual es
+              extraída.
             </p>
           </li>
           <li>
             <p>
-              La pulpa de fruta se utiliza para la elaboración de jugos naturales, batidos, heladería, confitería, refrescos, conservas, postres, cocteles, mermeladas, etc.
+              La pulpa de fruta se utiliza para la elaboración de jugos
+              naturales, batidos, heladería, confitería, refrescos, conservas,
+              postres, cocteles, mermeladas, etc.
             </p>
-          </li>
-          <li>
-            <p>Usar pulpa de fruta optimiza y ahorra tiempo en el proceso de elaboración de los diferentes productos, economiza dinero y evita desperdicios</p>
           </li>
           <li>
             <p>
-              Permite disfrutar de ciertas frutas fuera de temporada.
+              Usar pulpa de fruta optimiza y ahorra tiempo en el proceso de
+              elaboración de los diferentes productos, economiza dinero y evita
+              desperdicios
             </p>
           </li>
           <li>
-            <p>Consumir frutas contribuye a mantener hábitos de vida saludable.</p>
+            <p>Permite disfrutar de ciertas frutas fuera de temporada.</p>
           </li>
           <li>
             <p>
-            Contribuir en la cadena de producción y desarrollo del campo colombiano.
+              Consumir frutas contribuye a mantener hábitos de vida saludable.
             </p>
           </li>
-
+          <li>
+            <p>
+              Contribuir en la cadena de producción y desarrollo del campo
+              colombiano.
+            </p>
+          </li>
         </ul>
       </div>
       <div className="values ">
-        <h1 className="tittlequienes" style={{ color: product.main_color }}>Valores</h1>
+        <h1 className="tittlequienes" style={{ color: product.main_color }}>
+          Valores
+        </h1>
         <div className="holder">
           <img src="./images/Frame 1.png" alt="Logo" />
           <div className="container">
-            <h2 className="tittlequienes" >COMPROMISO</h2>
+            <h2 className="tittlequienes">COMPROMISO</h2>
             <p>Hacer todo bien con honestidad</p>
           </div>
           <div className="container">
-          <h2 className="tittlequienes" >CONFIANZA</h2>
+            <h2 className="tittlequienes">CONFIANZA</h2>
             <p>Convicción propia para alcanzar los objetivos trazados</p>
-            
           </div>
           <div className="container">
-          <h2 className="tittlequienes">EXCELENCIA</h2>
-            <p>Obtener la máxima eficacia en la gestión para obtener los mejores resultados</p>
+            <h2 className="tittlequienes">EXCELENCIA</h2>
+            <p>
+              Obtener la máxima eficacia en la gestión para obtener los mejores
+              resultados
+            </p>
           </div>
           <div className="container">
-          <h2 className="tittlequienes" >ÉTICA EMPRESARIAL</h2>
-            <p>Fomentar la convivencia y ambiente laboral con respeto y libertad</p>
+            <h2 className="tittlequienes">ÉTICA EMPRESARIAL</h2>
+            <p>
+              Fomentar la convivencia y ambiente laboral con respeto y libertad
+            </p>
           </div>
         </div>
       </div>
@@ -121,7 +135,6 @@ export const QuienesSomos = ({ product }) => {
         <ul className="objectives__list">
           <li>
             <p>
-              {" "}
               Garantizar el procesamiento del producto con buenas practica de
               manufactura (BPM)
             </p>
