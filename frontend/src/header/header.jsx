0b6726a74fc48588dd9ba2bfd7a41053e8ad0fff
CarlosLopez <carlosgablopezm@gmail.com>
2023-11-12 10:50:02 -0500

    fix: user logo in header

--- frontend/src/header/header.jsx
+++ frontend/src/header/header.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaBars, FaTimes, FaUserCircle, FaShoppingBasket } from "react-icons/fa";
 import "./header.css";
+import {AiOutlineUser} from "react-icons/ai";
 
 export const HeadPopUp = ({ product, trigger, togglePopup }) => {
   const menuRef = useRef();
@@ -118,7 +119,7 @@ export const Headercom = ({ product }) => {
                   <p onClick={() => logout()}>Salir</p>
                 </div>
                 <button id="userIcon" onClick={togglePopup}>
-                  <FaUserCircle />
+                  <AiOutlineUser />
                 </button>
                 <HeadPopUp
                   trigger={popup}
