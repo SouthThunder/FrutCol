10cb7f8ce72d5a3e627512f236a8c3f3ea3e87a6
Luis Alejandro Vera Moreno <alejandrovemo@gmail.com>
2023-12-01 21:13:11 -0500

    updated: optimized register rendering and css

--- frontend/src/components/pages/registro/registro.jsx
+++ frontend/src/components/pages/registro/registro.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import "./registro.css";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { Toaster, toast } from "sonner";
+import "./registro.css";
 import axios from "axios";
 import jwt_decode from "jwt-decode";
-import { Toaster, toast } from "sonner";
 import GoogleLogin from "react-google-login";
 
 export const Registrocom = ({ refresh }) => {
@@ -145,13 +145,11 @@ export const Registrocom = ({ refresh }) => {
   };
 
   return (
-    <div className="registrocontain">
-      <div className="contderform">
+    <div className="contderformlogin">
+      <div className="form">
         <h1>Registro</h1>
-        <br />
-
-        <div className="form">
-          <label htmlFor="nombre">Nombre:</label>
+        <p htmlFor="nombre">Nombre:</p>
+        <div className="password">
           <input
             type="text"
             id="nombre"
@@ -159,9 +157,10 @@ export const Registrocom = ({ refresh }) => {
             onChange={handleInputChange}
             value={formData.nombre}
           />
-          <br />
+        </div>
 
-          <label htmlFor="apellido">Apellido:</label>
+        <p htmlFor="apellido">Apellido:</p>
+        <div className="password">
           <input
             type="text"
             id="apellido"
@@ -169,9 +168,10 @@ export const Registrocom = ({ refresh }) => {
             onChange={handleInputChange}
             value={formData.apellido}
           />
-          <br />
+        </div>
 
-          <label htmlFor="cedula">Cédula:</label>
+        <p htmlFor="cedula">Cédula:</p>
+        <div className="password">
           <input
             type="number"
             id="cedula"
@@ -180,9 +180,10 @@ export const Registrocom = ({ refresh }) => {
             onChange={handleInputChange}
             value={formData.cedula}
           />
-          <br />
+        </div>
 
-          <label htmlFor="direccion">Dirección:</label>
+        <p htmlFor="direccion">Dirección:</p>
+        <div className="password">
           <input
             type="text"
             id="direccion"
@@ -190,9 +191,10 @@ export const Registrocom = ({ refresh }) => {
             onChange={handleInputChange}
             value={formData.direccion}
           />
-          <br />
+        </div>
 
-          <label htmlFor="correo">Correo:</label>
+        <p htmlFor="correo">Correo:</p>
+        <div className="password">
           <input
             type="email"
             id="correo"
@@ -200,9 +202,10 @@ export const Registrocom = ({ refresh }) => {
             onChange={handleInputChange}
             value={formData.correo}
           />
-          <br />
+        </div>
 
-          <label htmlFor="contrasena">Contraseña:</label>
+        <p htmlFor="contrasena">Contraseña:</p>
+        <div className="password">
           <input
             type="password"
             id="contrasena"
@@ -210,25 +213,26 @@ export const Registrocom = ({ refresh }) => {
             onChange={handleInputChange}
             value={formData.contrasena}
           />
-          <br />
-          <div className="terms_coditions">
-            <input
-              type="checkbox"
-              name="terminos&condiciones"
-              onChange={() => setTermsAccepted(!termsAccepted)}
-              checked={termsAccepted}
-            />
-            <label htmlFor="terminos&condiciones">
-              He leído y acepto los{" "}
-              <Link to="/Privacidad">términos y condiciones</Link>
-            </label>
-          </div>
-          <button type="submit" onClick={handleSubmit}>
+        </div>
+        <div className="terms_coditions">
+          <input
+            type="checkbox"
+            name="terminos&condiciones"
+            onChange={() => setTermsAccepted(!termsAccepted)}
+            checked={termsAccepted}
+          />
+          <p htmlFor="terminos&condiciones">
+            He leído y acepto los{" "}
+            <Link to="/Privacidad">términos y condiciones</Link>
+          </p>
+        </div>
+        {formError && <p className="error">{formError}</p>}
+
+        <div className="send">
+          <button className="enviar" onClick={handleSubmit}>
             Registrarse
           </button>
         </div>
-
-        {formError && <p className="error">{formError}</p>}
         <div className="googleAuth">
           <GoogleLogin
             clientId="336496153339-bfh9gkv3l2ktbgnq5725nba8kp84u5ff.apps.googleusercontent.com"
@@ -238,33 +242,13 @@ export const Registrocom = ({ refresh }) => {
             cookiePolicy={"single_host_origin"}
           />
         </div>
-        <Toaster richColors />
+        <p className="registro">
+          ¿Ya tienes una cuenta?{" "}
+          <span onClick={() => navigate("/ingreso")} onKeyDown={() => navigate('/ingreso')}>Inicia Sesión</span>
+        </p>
       </div>
 
-      <div className="contizform">
-        <div className="logo-container">
-          <Link to={"/"} className="nav__logo">
-            <img src="images/Frame 1.png" alt="" />
-            <p
-              style={{
-                transition: "all 1s var(--btn-cubic-bezier)",
-              }}
-            >
-              FrutCol
-            </p>
-          </Link>
-        </div>
-
-        <h1>¿Ya tienes cuenta?</h1>
-        <p>Si es así, inicia sesión con nosotros</p>
-        <br />
-        <br />
-        <br />
-
-        <Link to={"/Ingreso"}>
-          <button className="botonizform">Iniciar sesión</button>
-        </Link>
-      </div>
+      <Toaster richColors />
     </div>
   );
 };
